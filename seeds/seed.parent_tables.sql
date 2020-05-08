c0ec46d75595f284c4fab3a21c1db68f6c811f35
Anar Chalk <annachak9@gmail.com>
2020-05-08 17:06:13 -0400

    fixed patch req, added services and router

--- seeds/seed.parent_tables.sql
+++ seeds/seed.parent_tables.sql
@@ -14,11 +14,11 @@ VALUES
   ('lexlor', 'Alex Taylor', 'lex-password'),
   ('wippy', 'Ping Won In', 'ping-password');
 
-INSERT INTO events (parent_name, title, description, time_of_event)
+INSERT INTO events (parent_name, title, description, address, time_of_event, type)
 VALUES
-  ('Madie','Weekend Picnic', 'I am having a picnic this weekend', '2020-05-10 13:30:00'),
-  ('Meek','Play Date', 'Welcome to our playdate this Friday', '2020-05-15 14:45:00'),
-  ('Jasmine','Tutoring', 'I need someone to tutor my 8 year old son', '2020-05-20 17:00:00');
+  ('Madie','Weekend Picnic', 'I am having a picnic this weekend','Culver city park 300 Jefferson blvd, Culver City, CA 90232.', '2020-05-10 13:30:00','outdoor-activities' ),
+  ('Meek','Play Date', 'Welcome to our playdate this Friday', 'Central Park, 110 Central Park S. New York, NY 10019.', '2020-05-15 14:45:00', 'music-dance'),
+  ('Jasmine','Tutoring', 'I need someone to tutor my 8 year old son', 'Little Library, 209 Pacific Palisades, Santa Monica, CA90405.','2020-05-20 17:00:00', 'tutoring');
 
 
 COMMIT;
