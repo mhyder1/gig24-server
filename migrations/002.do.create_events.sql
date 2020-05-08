3e70460682879c3c3a84a47c9c1fcc4ed702c316
Anar Chalk <annachak9@gmail.com>
2020-05-07 21:42:38 -0400

    added knex and pg

--- migrations/002.do.create_events.sql
+++ migrations/002.do.create_events.sql
@@ -1,5 +1,6 @@
 CREATE TABLE events (
   id SERIAL PRIMARY KEY,
+  parent_name TEXT NOT NULL,
   title TEXT NOT NULL,
   description TEXT NOT NULL,
   time_of_event TIMESTAMPTZ DEFAULT now() NOT NULL
--- seeds/seed.parent_tables.sql
+++ seeds/seed.parent_tables.sql
@@ -14,11 +14,11 @@ VALUES
   ('lexlor', 'Alex Taylor', 'lex-password'),
   ('wippy', 'Ping Won In', 'ping-password');
 
-INSERT INTO events (title, description, time_of_event)
+INSERT INTO events (parent_name, title, description, time_of_event)
 VALUES
-  ('Weekend Picnic', 'I am having a picnic this weekend', '2020-05-10 13:30:00'),
-  ('Play Date', 'Welcome to our playdate this Friday', '2020-05-15 14:45:00'),
-  ('Tutoring', 'I need someone to tutor my 8 year old son', '2020-05-20 17:00:00');
+  ('Madie','Weekend Picnic', 'I am having a picnic this weekend', '2020-05-10 13:30:00'),
+  ('Meek','Play Date', 'Welcome to our playdate this Friday', '2020-05-15 14:45:00'),
+  ('Jasmine','Tutoring', 'I need someone to tutor my 8 year old son', '2020-05-20 17:00:00');
 
 
 COMMIT;
