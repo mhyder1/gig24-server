d3b669d485bba306e36317b1362965e18700e309
Anar Chalk <annachak9@gmail.com>
2020-05-11 23:10:58 -0400

    updated seed, migration files, added attendee tables and implemented logic

--- seeds/seed.parent_tables.sql
+++ seeds/seed.parent_tables.sql
@@ -2,6 +2,7 @@ BEGIN;
 
 TRUNCATE
   users,
+  attend,
   events;
   -- RESTART IDENTITY CASCADE;
 
@@ -20,4 +21,13 @@ VALUES
   ('Meek','Play Date', 'Welcome to our playdate this Friday', 'Central Park, 110 Central Park S. New York, NY 10019.', '2020-05-15 14:45:00', 'music-dance', 2),
   ('Jasmine','Tutoring', 'I need someone to tutor my 8 year old son', 'Little Library, 209 Pacific Palisades, Santa Monica, CA90405.','2020-05-20 17:00:00', 'tutoring', 3);
 
+INSERT INTO attend (guest, event, children)
+VALUES
+  (1,2,2),
+  (1,3,1),
+  (2,1,1),
+  (2,3,4),
+  (3,1,1),
+  (3,2,3);
+  
 COMMIT;
